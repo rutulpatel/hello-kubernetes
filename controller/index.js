2ff8b36af3eab7843e1cca53f6b4be1044c8ef81
Rutul Patel <rutulpatel@Rutuls-MacBook-Pro.local>
2018-05-05 12:13:42 -0500

    initial commit with working app

---
+++ controller/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+var os = require("os");
+
+var VERSION = "1.0";
+
+router.get("/", function(req, res) {
+    var response = "<h1>Hello Kubernetes</h1>" +
+                    "<h3>Running on host: " + os.hostname() + "</h3>"  +
+                    "<h3>Version: " + VERSION + "</h3>";
+    res.send(response);
+});
+
+module.exports = router;
